eacc04b64e160874678cc3a33b01bbd225ed54dd
sowmyashettym <78196390+sowmyashettym@users.noreply.github.com>
2025-06-28 17:55:31 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,19 +10,20 @@ def driver:
   yield driver
   driver.quit()
 
-@pytest.hookimpl(hookwarapper=True)
+@pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item,call):
   outcome=yield
   result=outcome.get_result()
 
-if result.when =="failed" and result =="call":
+if result.when =="call" and result.failed:
   driver = item.funcargs.get("driver")
   if driver :
       timestr = datetime.now().strftime("%y_%m_%d_%H_%M_%S)
       screenshots_dir = os.path.join(os.getcwd(),"screenshots")
       os.makedirs(screenshots_dir,exist_ok=True)
       file_name = f"failed_{timestr}.png"
-      os.path.join(screenshots_dir,file_name")
+      filepath = os.path.join(screenshots_dir,file_name)
+      driver.save_screenshot(filepath)
       pytest_html = item.config.pluginmanager.get_plugin("html")
       if pytest_html:
           extra = getattr(result,"extra",[])
